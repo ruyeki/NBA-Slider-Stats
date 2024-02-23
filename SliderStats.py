fae184b180e1170f7ca5c4b4632ee617535d1677
NobodyTypical <diazjuan1029@gmail.com>
2024-02-22 19:12:36 -0800

    Moved everything to separate pages along with creating a new head to head comparison feature

---
+++ SliderStats.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+def main():
+    st.title('Welcome to SliderStats!')
+    st.write('SliderStats allows you to explore data using sliders.')
+
+if __name__ == "__main__":
+    main()
